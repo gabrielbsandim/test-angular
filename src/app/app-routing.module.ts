a7b4402d9a904cc8dc5983ce52fcceae4fa85619
Gabriel Bastos Sandim <gabriel.bsandim@gmail.com>
2021-06-23 23:53:12 -0300

    # Feat: configure initial routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UnitListComponent } from './pages/unit-list/unit-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/unit-list', pathMatch: 'full' },
+  { path: 'unit-list', component: UnitListComponent, }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
