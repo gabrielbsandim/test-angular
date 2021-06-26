46597ed14cf40aa8c6dbcbffc8c68d2c4bdc11ee
Gabriel Bastos Sandim <gabriel.bsandim@gmail.com>
2021-06-26 20:03:50 -0300

    # Feat: add route to business-detail

--- src/app/pages/business-list/business-list.component.ts
+++ src/app/pages/business-list/business-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 import { IBusiness } from 'src/app/models/business.model';
 import { BusinessService } from 'src/app/services/business.service';
 import { FilterBusiness } from 'src/app/utils/filterBusiness.util';
@@ -12,16 +13,15 @@ import { FilterBusiness } from 'src/app/utils/filterBusiness.util';
   styleUrls: ['./business-list.component.scss']
 })
 export class BusinessListComponent implements OnInit {
-  private businessList: IBusiness[] = [];
   public displayedColumns: string[] = ['name', 'business', 'valuation', 'active', 'id'];
   public businessListFilteredDataSource = new MatTableDataSource<IBusiness>();
-
+  private businessList: IBusiness[] = [];
   private filterBusiness = new FilterBusiness()
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(private businessService: BusinessService) { }
+  constructor(private businessService: BusinessService, private router: Router) { }
 
   ngOnInit() {
     this.businessService.fetchAllBusiness().subscribe((resFetchBusiness) => {
@@ -48,6 +48,6 @@ export class BusinessListComponent implements OnInit {
   }
 
   public openBusinessDetail(businessId: string) {
-    console.log(businessId)
+    this.router.navigateByUrl(`/business-detail/${businessId}`)
   }
 }
