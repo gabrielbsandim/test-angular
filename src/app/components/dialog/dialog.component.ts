422a3d90df4c083ee4b8dd1609ce29d39af0c9e3
Gabriel Bastos Sandim <gabriel.bsandim@gmail.com>
2021-06-26 20:01:52 -0300

    # Feat: add dialog component

---
+++ src/app/components/dialog/dialog.component.ts
@@ -0,0 +1,23 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface IDialogData {
+  header: string
+}
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss']
+})
+export class DialogComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: IDialogData
+  ) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
